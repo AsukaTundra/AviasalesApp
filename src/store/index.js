bb9904d17ecae92bad2e6fd874cccc475309b145
AsukaTundra <c.vodopyanov@mail.ru>
2024-03-12 12:12:22 +0300

    2.1

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { compose } from "redux";
-import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+// import { compose } from "redux";
+// import { applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 import AppSlice from "./app-slice";
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+// const composeEnhancers =
+//   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
 
-const store = configureStore({ reducer: { AppSlice } }, composeEnhancers(applyMiddleware()));
+const store = configureStore({ reducer: { AppSlice } });
 export default store;
